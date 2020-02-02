213d4f25a904b0a0878b6aa59e21377bc97cc4d5
Chuck Heazel <cheazel@wiscenterprises.com>
2020-02-02 10:31:46 -0500

    January 2020 Update
    Data Dictionary with included requirements generated from UML model.  Assorted tools to automate generation of requirements and abstract tests.

---
+++ tools/Building-gen.sh
@@ -0,0 +1,73 @@
+#
+# A tool to generate AsciiDoc formatted Requirements and Abstract Test files for 
+# the CityGML 3.0 Conceptual Model. A copy of this script should be created for 
+# each package. The only difference is in the values of the variables.
+#
+# This tool uses a set of template files, one for each class type. Sed commands 
+# replace tokens in the template with values from the script variables. The script 
+# variables are also used to build the file names for the output.
+#
+# Created by Chuck Heazel
+# January, 2020
+# 
+
+# First define the variables
+# The package name
+PKG=Building
+
+# A list of the Abstract Classes
+ACLIST="AbstractBuilding AbstractBuildingSubdivision"
+
+# The instantiatable classes
+CCLIST="Building BuildingConstructiveElement BuildingFurniture BuildingInstallation BuildingPart BuildingRoom BuildingUnit Storey"
+
+# CodeLists
+CLLIST="BuildingClassValue BuildingConstructiveElementClassValue BuildingConstructiveElementFunctionValue BuildingConstructiveElementUsageValue BuildingFunctionValue BuildingFurnitureClassValue BuildingFurnitureFunctionValue BuildingFurnitureUsageValue BuildingInstallationClassValue BuildingInstallationFunctionValue BuildingInstallationUsageValue BuildingRoomClassValue BuildingRoomFunctionValue BuildingRoomUsageValue BuildingSubdivisionClassValue BuildingSubdivisionFunctionValue BuildingSubdivisionUsageValue BuildingUsageValue RoofTypeValue RoomElevationReferenceValue"
+
+# And Data Types
+DTLIST="RoomHeight" 
+# Then for each list of classes, the substitution commands are run gainst the 
+# appropriate requirement template.
+
+for I in $ACLIST
+do
+cat REQ_Package_Abstract_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/ | sed s/"REFF"/"<<"$I/ >../standard/requirements/$PKG/REQ_`echo $PKG`_$I.adoc
+done
+
+for I in $CCLIST
+do
+cat REQ_Package_Class_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/ | sed s/"REFF"/"<<"$I/ >../standard/requirements/$PKG/REQ_`echo $PKG`_$I.adoc
+done
+
+for I in $CLLIST
+do
+cat REQ_Package_CodeList_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/ | sed s/"REFF"/"<<"$I/ >../standard/requirements/$PKG/REQ_`echo $PKG`_$I.adoc
+done
+
+for I in $DTLIST
+do
+cat REQ_Package_BasicType_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/ | sed s/"REFF"/"<<"$I/ >../standard/requirements/$PKG/REQ_`echo $PKG`_$I.adoc
+done
+
+# Now do the same thing for the Abstract Test templates
+
+for I in $ACLIST
+do
+cat AT_Package_Abstract_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/g | sed s/"REFF"/"<<"$I/ >../standard/abstract_tests/$PKG/ATS_`echo $PKG`_$I.adoc
+done
+
+for I in $CCLIST
+do
+cat AT_Package_Class_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/g | sed s/"REFF"/"<<"$I/ >../standard/abstract_tests/$PKG/ATS_`echo $PKG`_$I.adoc
+done
+
+for I in $CLLIST
+do
+cat AT_Package_CodeList_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/g | sed s/"REFF"/"<<"$I/ >../standard/abstract_tests/$PKG/ATS_`echo $PKG`_$I.adoc
+done
+
+for I in $DTLIST
+do
+cat AT_Package_BasicType_Template.adoc | sed s/"CLASS"/$I/g | sed s/"PACKAGE"/$PKG/g | sed s/"REFF"/"<<"$I/ >../standard/abstract_tests/$PKG/ATS_`echo $PKG`_$I.adoc
+done
+
