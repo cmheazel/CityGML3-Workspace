aaaf6483c6ae5d7caddb33fafaeb550e43eb0af6
Chuck Heazel <cheazel@wiscenterprises.com>
2020-07-09 13:27:43 -0400

    Initial draft of the Users Guide

--- best_practice/HTML_Gen.bat
+++
@@ -1,2 +0,0 @@
-asciidoctor --trace -o ../19-072BP.html CityGML_Conceptual_Model_BP.adoc
-
--- best_practice/PDF_Gen.bat
+++
@@ -1,2 +0,0 @@
-asciidoctor -r asciidoctor-pdf --trace -b pdf -o ..\19-072BP.pdf CityGML_Conceptual_Model_BP.adoc
-
